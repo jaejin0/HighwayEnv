ff536800a2caf2a71730352867d56b444cafc96f
Jaejin Cha <jaejin_cha@wifi-roaming-128-4-71-153.wifi.udel.edu>
2023-06-29 15:21:28 -0400

    Changed TAU_HEADING from 0.8 to 0.02 in controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -21,7 +21,7 @@ class ControlledVehicle(Vehicle):
 
     """Characteristic time"""
     TAU_ACC = 0.6  # [s]
-    TAU_HEADING = 0.8  # [s]
+    TAU_HEADING = 0.02  # [s]
     TAU_LATERAL = 0.6  # [s]
 
     TAU_PURSUIT = 0.5 * TAU_HEADING  # [s]
