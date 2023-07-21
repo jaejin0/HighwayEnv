ccccabd1382ee4e12a275599777d106baae962b3
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-07-21 16:43:55 -0400

    trajectory class test 17

--- highway_env/vehicle/graphics.py
+++ highway_env/vehicle/graphics.py
@@ -171,11 +171,16 @@ class VehicleGraphics(object):
         headlight_length, headlight_width = 0.72, 0.6
         roof_length, roof_width = 2.0, 1.5
 
+        point_side = 3
+
         # Vehicle rectangle
         length = v.WIDTH
         vehicle_surface = pygame.Surface((surface.pix(length), surface.pix(length)),
                                          flags=pygame.SRCALPHA)  # per-pixel alpha
-        rect = (1,1,1,1)
+        rect = (surface.pix(tire_length),
+                surface.pix(length / 2 - v.WIDTH / 2),
+                surface.pix(point_side),
+                surface.pix(point_side))
         rect_headlight_left = (surface.pix(tire_length+v.LENGTH-headlight_length),
                                surface.pix(length / 2 - (1.4*v.WIDTH) / 3),
                                surface.pix(headlight_length),
