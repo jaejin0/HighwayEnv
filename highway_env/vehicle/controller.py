de8adfe63aeecd2be929a807ef87413774b855e0
Jaejin Cha <jaejin_cha@wifi-roaming-128-4-140-87.wifi.udel.edu>
2023-07-25 23:35:01 -0400

    trajectory class test 109

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -519,13 +519,12 @@ class TrajectoryVehicle(Vehicle):
         
         points = []
         cur_pt = copy.deepcopy(self.position)
-        print(self.heading)
-        cur_angle = copy.deepcopy(self.heading)
+        # print(self.heading)
+        # cur_angle = copy.deepcopy(self.heading)
         for i in range(len(distances)):
             dis = distances[i]
-            
-            cur_angle = angles[i]
-            cur_pt = cur_pt + np.array([np.cos(cur_angle), np.sin(cur_angle)]) * dis
+            angle = angles[i]
+            cur_pt = cur_pt + np.array([np.cos(angle), np.sin(angle)]) * dis
             
             points.append(cur_pt)
         
