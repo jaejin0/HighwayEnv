81d15b7925be92ff6e8269a753e6c742852a9199
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-07-31 13:27:17 -0400

    test trajectory following 4

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -432,7 +432,9 @@ class TrajectoryVehicle(Vehicle):
             # set target to the next one.
             diff_x, diff_y = x - self.trajectory_points[self.trajectory_index][0], y - self.trajectory_points[self.trajectory_index][1]
             diff_angle = math.atan(diff_y / diff_x)
+            print("___________")
             print(diff_angle)
+            print(self.heading)
             
             #  self.trajectory_index < len(self.trajectory_distances):
             #     diff_x, diff_y = x - self.trajectory_points[self.trajectory_index][0], y - self.trajectory_points[self.trajectory_index][1]
