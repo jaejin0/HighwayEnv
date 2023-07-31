5a229558bec5a02ce1c8c5fd318114ae93739cf0
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-07-31 15:43:32 -0400

    test trajectory following 31

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -447,6 +447,7 @@ class TrajectoryVehicle(Vehicle):
                 
                 _x, _y = self.target_x - x, self.target_y - y
                 self.target_steering_angle = math.atan2(_y, _x)
+                print(self.target_steering_angle)
                 self.target_steering_angle = self.trajectory_angles[self.trajectory_index]
             
             else:
