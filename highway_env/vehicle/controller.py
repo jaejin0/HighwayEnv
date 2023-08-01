ded93465442a6d950ba267746a1e354f82c29f13
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-08-01 16:05:06 -0400

    test trajectory following 86

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -425,9 +425,9 @@ class TrajectoryVehicle(Vehicle):
             self.target_steering_angle = target_angle
         elif angle_diff >= 3 * np.pi / 2:
             
-            self.target_steering_angle = target_angle
+            self.target_steering_angle = target_angle - 2 * np.pi
         elif angle_diff <= - 3 * np.pi / 2:
-            self.target_steering_angle = target_angle
+            self.target_steering_angle = target_angle + 2 *np.pi
         else:
             if self.trajectory_index < len(self.trajectory_distances) - 1:
                 self.trajectory_index += 1
