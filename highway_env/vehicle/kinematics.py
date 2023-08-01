b918fe4e6618224fc6229e56c006ed5a5731d25a
Jaejin Cha <jaejin_cha@wifi-roaming-128-4-132-51.wifi.udel.edu>
2023-08-01 18:29:28 -0400

    test angle 14

--- highway_env/vehicle/kinematics.py
+++ highway_env/vehicle/kinematics.py
@@ -131,6 +131,7 @@ class Vehicle(RoadObject):
         self.heading += self.speed * np.sin(beta) / (self.LENGTH / 2) * dt
         self.heading = utils.wrap_to_pi(self.heading)
         self.speed += self.action['acceleration'] * dt
+        print(self.speed)
         self.on_state_update()
 
     def clip_actions(self) -> None:
