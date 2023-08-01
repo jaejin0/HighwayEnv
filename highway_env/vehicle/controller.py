83b97ec4fc059d5e6d55070b86f7d202983bf5a9
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-08-01 14:27:26 -0400

    test trajectory following 56

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -418,8 +418,8 @@ class TrajectoryVehicle(Vehicle):
         _x, _y = self.target_x - x, self.target_y - y
         
         target_angle = math.atan2(_y, _x)
-        angle_diff = abs(target_angle + self.heading)
-        print(self.heading)
+        angle_diff = abs(target_angle - ( -self.heading / np.pi))
+        print(angle_diff)
        
         
         if angle_diff <= np.pi / 2: 
