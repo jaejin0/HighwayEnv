aa808ebcdbcd62f779627b362bf6ba3286cb18ec
Jaejin Cha <jaejin_cha@wifi-roaming-128-4-132-51.wifi.udel.edu>
2023-08-02 15:38:54 -0400

    observation test 2

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -28,7 +28,7 @@ class HighwayEnv(AbstractEnv):
             "observation": {
                 "type": "Kinematics",
                 "vehicles_count": 10,
-                "features": ["presence", "x", "y", "vx", "vy", "heading", "cos_d", "sin_d"],
+                "features": ["presence", "x", "y", "vx", "vy", "heading"],
             },
             "action": {
                 "type": "TrajectoryAction",
