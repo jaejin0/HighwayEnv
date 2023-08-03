1bde43467ce77d6daedaf92d6e3bdd88dcc2c1a1
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-08-03 19:40:45 -0400

    reward test 51

--- highway_env/vehicle/kinematics.py
+++ highway_env/vehicle/kinematics.py
@@ -122,7 +122,7 @@ class Vehicle(RoadObject):
 
         :param dt: timestep of integration of the model [s]
         """ 
-        self.action['acceleration'] = -5
+        print(self.acceleration)
         self.clip_actions()
         beta = self.action['steering']
         
@@ -137,7 +137,6 @@ class Vehicle(RoadObject):
         self.heading = utils.wrap_to_pi(self.heading)
         self.speed += self.action["acceleration"] * dt
         self.on_state_update()
-        print(self.speed)
 
     def clip_actions(self) -> None:
         if self.crashed:
