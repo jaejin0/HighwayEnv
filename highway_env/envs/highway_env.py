3d4b1d67ebba75741b810444c3c3da038c577292
Jaejin Cha <jaejin_cha@Jaejins-MacBook-Pro.local>
2023-08-03 20:28:43 -0400

    reward test 68

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -40,17 +40,17 @@ class HighwayEnv(AbstractEnv):
             "duration": 40,  # [s]
             "ego_spacing": 2,
             "vehicles_density": 1,
-            # "collision_reward": -1,    # The reward received when colliding with a vehicle.
-            # "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
-            #                            # zero for other lanes.
-            # "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
-            #                            # lower speeds according to config["reward_speed_range"].
-            # "lane_change_reward": 0,   # The reward received at each lane change action.
+            "collision_reward": -1,    # The reward received when colliding with a vehicle.
+            "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
+                                       # zero for other lanes.
+            "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
+                                       # lower speeds according to config["reward_speed_range"].
+            "lane_change_reward": 0,   # The reward received at each lane change action.
             
             
-            # "reward_speed_range": [-40, 40],
-            # "normalize_reward": True,
-            # "offroad_terminal": False
+            "reward_speed_range": [-40, 40],
+            "normalize_reward": True,
+            "offroad_terminal": False,
             
             
             ### Speed ###   25
