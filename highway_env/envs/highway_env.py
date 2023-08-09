b066d2ab29654644f007d8236be61268995d1985
Jaejin Cha <jaejin_cha@wifi-roaming-128-4-132-51.wifi.udel.edu>
2023-08-09 00:18:23 -0400

    returning info about reward

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -65,12 +65,12 @@ class HighwayEnv(AbstractEnv):
             "rear_distance_range": [0, 30],
             
             ### Energy Saving ###  0
-            "torque_reward": -75,
+            "torque_reward": -33,
             
             "total_torque_range": [23.64, 89.64],
             
             # Reward Range #
-            "reward_range": [-175, 200]
+            "reward_range": [-133, 200]
             
         })
         return config
